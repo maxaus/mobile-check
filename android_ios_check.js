bb9a1234abe539311cccab84e5c05554e462b4b6
Dmitriy Vovney <dvovney@noveogroup.com>
2015-01-22 11:35:59 +0600

    Another test

--- android_ios_check.js
+++ android_ios_check.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             window.location = "twitter://";
         }
         else if (isAndroid()) {
-
+            window.location = "intent://scan/#Intent;scheme=zxing;package=com.google.zxing.client.android;end";
         }
     }, 5000);
 
